c66a4cd7d444e83cff1b7831144cc7777d00a1b3
♪ hiljusti 🎮 <hiljusti@amazon.com>
2020-12-03 21:56:33 -0800

    Add word boundaries

--- day-04/solve2.js
+++ day-04/solve2.js
@@ -4,16 +4,16 @@ const {readFileSync} = require('fs');
 // Splitting key from value and parsing ints would
 // be more readable (and performant?) for many of these.
 const patterns = [
-    /byr:(19[2-9]\d|200[012])/,
-    /iyr:20(1\d|20)/,
-    /eyr:20(2\d|30)/,
-    /hgt:(1([5-8]\d|9[0123])cm|(59|6\d|7[0-6])in)/,
-    /hcl:#[0-9a-f]{6}/,
-    /ecl:(amb|blu|brn|gry|grn|hzl|oth)/,
-    /pid:\d{9}/
+    /\bbyr:(19[2-9]\d|200[012])\b/,
+    /\biyr:20(1\d|20)\b/,
+    /\beyr:20(2\d|30)\b/,
+    /\bhgt:(1([5-8]\d|9[0123])cm|(59|6\d|7[0-6])in)\b/,
+    /\bhcl:#[0-9a-f]{6}\b/,
+    /\becl:(amb|blu|brn|gry|grn|hzl|oth)\b/,
+    /\bpid:\d{9}\b/
 ];
 
-const validate = pass => patterns.every(pass => e.match(p));
+const validate = pass => patterns.every(patt => pass.match(patt));
 
 const n = readFileSync("./input", "utf8")
     .replace(/\n/g, " ")
