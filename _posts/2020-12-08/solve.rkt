e56cd1232127acb117a6bb3eb52a9f283651d665
🎧hiljusti🕹 <hiljusti@amazon.com>
2020-12-08 17:09:48 -0800

    Solve part 2

--- _posts/2020-12-08/solve.rkt
+++ _posts/2020-12-08/solve.rkt
@@ -2,20 +2,41 @@
 #lang racket
 
 (define the-code
-    (make-hash (sequence->list (sequence-map (lambda (e i) (list i (first e) (string->number (second e))))
+    (make-immutable-hash (sequence->list (sequence-map (lambda (e i) (list i (first e) (string->number (second e))))
         (in-indexed (filter-map (lambda (s) (and (non-empty-string? s) (string-split s " ")))                                                   (file->lines "input")))))))
 
-(define (compute-line add acc code seen)
-    (if (set-member? seen add)
-        (error "Attempted to traverse to already seen address: ~a" add)
-        (let* ([seen (set-add seen add)]
-               [line (hash-ref code add)]
-               [op (first line)]
-               [n (second line)])
-            (printf "~a: ~a ~a | ~a\n" add op n acc)
-            (let ([add (case op [("jmp") (+ add n)] [else (add1 add)])]
-                  [acc (case op [("acc") (+ acc n)] [else acc])])
-                (compute-line add acc code seen)))))
+(define (compute-line addr acc code seen)
+    (cond
+        [(set-member? seen addr)
+            (eprintf "Error: Attempted to traverse to already seen address\naddr: ~a\nacc: ~a\n\n" addr acc)
+            #f]
+        [(hash-has-key? code addr)
+            (let* ([seen (set-add seen addr)]
+                   [line (hash-ref code addr)]
+                   [op (first line)]
+                   [n (second line)])
+                (printf "~a: ~a ~a | ~a\n" addr op n acc)
+                (let ([addr (case op [("jmp") (+ addr n)] [else (add1 addr)])]
+                      [acc (case op [("acc") (+ acc n)] [else acc])])
+                    (compute-line addr acc code seen)))]
+        [else (printf "Final result: ~a\n\n" acc) #t]))
 
+; Part 1, expected to error out.
 (compute-line 0 0 the-code (set))
+
+(print "Type something to continue.")
+(read)
+
+; Part 2, searching for a non-error case 
+; We can flip one jmp to nop or one nop to jmp.
+(define swap (hash "jmp" "nop" "nop" "jmp"))
+(define swapped-codes
+    (filter-map
+        (lambda (line)
+            (let ([add (first line)] [op (second line)] [n (third line)])
+                (and (hash-has-key? swap op) (hash-set the-code add (list (hash-ref swap op) n)))))
+        (hash->list the-code)))
+
+(and (findf (lambda (swapped-code) (compute-line 0 0 swapped-code (set))) swapped-codes)
+     (printf "Done.\n"))
 
