4c848eb181e2381f2bd30a2808d9b9f571364629
♪ hiljusti 🎮 <hiljusti@amazon.com>
2020-12-15 11:15:48 -0800

    refactor

--- _posts/2020-12-13/solve.lisp
+++ _posts/2020-12-13/solve.lisp
@@ -5,19 +5,20 @@
         (busses-raw (substitute #\SPACE #\, (read-line input))))
     (defvar timestamp (parse-integer timestamp-raw))
     (defvar busses
-      (remove 0 (read-from-string
-                  (concatenate 'string "(" (substitute #\0 #\x busses-raw) ")"))))))
+      (read-from-string
+                  (concatenate 'string "(" (substitute #\0 #\x busses-raw) ")")))))
 
 (defun next-after-timestamp (n)
     (list (- n (mod timestamp n)) n))
 (defun find-best-next (busses)
   (reduce (lambda (a b) (if (< (car b) (car a)) b a))
-          (map 'list #'next-after-timestamp busses)))
-(defun find-weird-thing (delay-bus)
-  (let ((delay (car delay-bus))
-        (bus-id (cadr delay-bus)))
-    (* bus-id delay)))
+          (map 'list #'next-after-timestamp (remove 0 busses))))
 
 ;;; Part 1 solution
-(format t "~a~%" (find-weird-thing (find-best-next busses)))
+(let* ((best-next-buss (find-best-next busses))
+       (bus-id (cadr best-next-buss))
+       (delay (car best-next-buss))
+       (weird-product (* bus-id delay)))
+  (format t "Product of bus id (~a) and delay (~a): ~a~%" 
+          bus-id delay weird-product))
 
