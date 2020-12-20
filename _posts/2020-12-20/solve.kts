7df93240782744bf678a2f047cada898885f0549
♪ hiljusti 🎮 <hiljusti@pm.me>
2020-12-20 12:00:06 -0800

    kotlin things

--- _posts/2020-12-20/solve.kts
+++ _posts/2020-12-20/solve.kts
@@ -7,31 +7,58 @@ class Tile(raw: String) {
     val n: BigInteger
     val borders: List<String>
     val rborders: List<String>
+    val tile: List<String>
 
     init {
         val lines = raw.split("\n")
         n = lines[0].replace(Regex("\\D"), "").toBigInteger()
+        tile = lines.drop(1).take(10)
         borders = listOf(
-            lines.drop(1)[0],
-            lines.drop(1).take(10).map { it[9] }.joinToString(""),
-            lines.drop(10)[0].reversed(),
-            lines.drop(1).take(10).map { it[0] }.joinToString("").reversed(),
+            tile[0],
+            tile.map { it[9] }.joinToString(""),
+            tile.drop(9)[0].reversed(),
+            tile.map { it[0] }.joinToString("").reversed(),
         )
         rborders = borders.map { it.reversed() }.reversed()
     }
 }
 
+fun edgesOf(tiles: List<Tile>): Set<String> =
+    tiles.flatMap { it.borders + it.rborders }
+        .groupBy { it }
+        .filterValues { it.size == 1 }
+        .keys
+
+fun cornersOf(tiles: List<Tile>, edges: Set<String> = edgesOf(tiles)): List<Tile> =
+    tiles.filter { it.borders.intersect(edges).size == 2 }
+
 val tiles: List<Tile> = File("input").readText()
     .split("\n\n")
     .filter { !it.isEmpty() }
     .map { Tile(it) }
-val edges: Set<String> = tiles.flatMap { it.borders + it.rborders }
-    .groupBy { it }
-    .filterValues { it.size == 1 }
-    .keys
-val corners: List<Tile> = tiles.filter { it.borders.intersect(edges).size == 2 }
-
-val solutionOne: BigInteger = corners.map { it.n }
+val edges: Set<String> = edgesOf(tiles)
+val solutionOne: BigInteger = cornersOf(tiles, edges).map { it.n }
     .fold(BigInteger.ONE, { a, b -> a * b })
 
+// Part 1 Solution
 println("Product of corner tile numbers: $solutionOne")
+
+fun seaify(seas: List<Tile>, startingCorner: Tile): List<String> {
+    val seaMap: MutableMap<BigInteger, Tile> = seas.associateBy { it.n }.toMutableMap()
+    seaMap.remove(startingCorner.n)
+    while (seaMap.isNotEmpty()) {
+        seas.mapIndexedNotNull { i, it ->
+            if ((it.borders + it.rborders).intersect(startingCorner.borders).size == 1 &&
+                it.n != startingCorner.n
+            ) { Pair(i, it) } else { null }
+        }
+        break
+    }
+    return listOf()
+}
+
+val seas: List<Tile> = tiles.filter { it.borders.intersect(edges).size == 0 }
+val seaCorner: Tile = cornersOf(seas)[0]
+println(seas)
+
+println(seaify(seas, seaCorner))
