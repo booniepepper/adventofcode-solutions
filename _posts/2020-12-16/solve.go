a8fbfc720e87dd47547fe5cd2ab93418d494809f
♪ hiljusti 🎮 <hiljusti@amazon.com>
2020-12-20 23:12:59 -0800

    Solve day 16 part 2

--- _posts/2020-12-16/solve.go
+++ _posts/2020-12-16/solve.go
@@ -28,20 +28,86 @@ func main() {
 	lines := strings.Split(string(input), "\n")
 
 	headers := parseHeaders(lines[0:NFields])
-	//myTicket := parseTicket(lines[NFields+2])
+	myTicket := parseTicket(lines[NFields+2])
 	tickets := parseTickets(lines[NFields+5:])
 
         invalidFields := findInvalidFields(headers, tickets)
 
+        // Part 1 solution
         fmt.Println("Sum of invalid fields:", sum(invalidFields))
+
+        validTickets := removeInvalids(headers, tickets)
+        orderedHeaders := orderHeaders(headers, validTickets)
+        departureFields := getDepartureFields(orderedHeaders)
+
+        // Part 2 solution
+        fmt.Println("Product of departure fields in my ticket:", product(selectByIndices(myTicket, departureFields)))
 }
 
-func sum(ns []int) int {
-    sum := 0
-    for _, n := range ns {
-        sum += n
+func getDepartureFields(headers []Header) []int {
+    fields := make([]int, 0)
+    for i, header := range headers {
+        if strings.HasPrefix(header.Name, "departure") {
+            fields = append(fields, i)
+        }
     }
-    return sum
+    return fields
+}
+
+func orderHeaders(unorderedHeaders []Header, tickets [][]int) []Header {
+    possibleHeaders := make([][]Header, NFields)
+
+    // Find all possible headers for a given field
+    for i := 0; i < NFields; i++ {
+        for _, header := range unorderedHeaders {
+            valid := true
+            for _, ticket := range tickets {
+                contained := false
+                for _, interval := range header.Intervals {
+                    if interval.Contains(ticket[i]) {
+                        contained = true
+                    }
+                }
+                valid = valid && contained
+            }
+            if valid {
+                possibleHeaders[i] = append(possibleHeaders[i], header)
+            }
+        }
+    }
+
+    // Filter down so there is only one header per field
+    filtered := false
+    for !filtered {
+        filtered = true
+        settleds := make([]bool, NFields)
+        for i, headers := range possibleHeaders {
+            settled := 1 == len(headers)
+            settleds[i] = settled
+            filtered = filtered && settled
+            if settled {
+                settledName := headers[0].Name
+                for j, unfilteredHeaders := range possibleHeaders {
+                    if j != i {
+                        filteredHeaders := make([]Header, 0)
+                        for _, unfilteredHeader := range unfilteredHeaders {
+                            if unfilteredHeader.Name != settledName {
+                                filteredHeaders = append(filteredHeaders, unfilteredHeader)
+                            }
+                        }
+                        possibleHeaders[j] = filteredHeaders
+                    }
+                }
+            }
+        }
+    }
+
+    headers := make([]Header, NFields)
+    for i, actualHeaders := range possibleHeaders {
+        headers[i] = actualHeaders[0]
+    }
+
+    return headers
 }
 
 func findInvalidFields(headers []Header, tickets [][]int) []int {
@@ -50,6 +116,25 @@ func findInvalidFields(headers []Header, tickets [][]int) []int {
        invalids = append(invalids, findInvalidField(headers, ticket)...)
     }
     return invalids
+}
+
+// Wow, I finally get the hubbub about why people want generics in Go.
+// No wonder the std library feels lower level rather than higher level.
+// (That is, they can't implement a generic filter, map, reduce etc. without ugly reflection hijinks)
+// https://blog.golang.org/generics-next-step
+// Whoa, Phil Wadler is on the case (Haskell monads and Java 5 Generics among other accomplishments)
+func removeInvalids(headers []Header, tickets [][]int) [][]int {
+    valids := make([][]int, 0)
+    for _, ticket := range tickets {
+        if isValidTicket(headers, ticket) {
+            valids = append(valids, ticket)
+        }
+    }
+    return valids
+}
+
+func isValidTicket(headers []Header, ticket []int) bool {
+    return 0 == len(findInvalidField(headers, ticket)) && len(ticket) == NFields
 }
 
 func findInvalidField(headers []Header, ticket []int) []int {
@@ -66,6 +151,33 @@ func findInvalidField(headers []Header, ticket []int) []int {
         }
     }
     return invalids
+}
+
+func selectByIndices(ns []int, is []int) []int {
+    selected := make([]int, 0)
+    for _, i := range is {
+        selected = append(selected, ns[i])
+    }
+    return selected
+}
+
+func product(ns []int) int {
+    if len(ns) == 0 {
+        return 0
+    }
+    product := 1
+    for _, n := range ns {
+        product *= n
+    }
+    return product
+}
+
+func sum(ns []int) int {
+    sum := 0
+    for _, n := range ns {
+        sum += n
+    }
+    return sum
 }
 
 func parseHeaders(rawHeaders []string) []Header {
