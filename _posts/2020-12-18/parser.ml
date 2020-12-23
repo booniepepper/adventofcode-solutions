6045b11ca088cfeeb6f957789d38754bfe8436ef
🎧hiljusti🕹 <hiljusti@amazon.com>
2020-12-23 15:46:43 -0800

    solve part 2!

--- _posts/2020-12-18/parser.ml
+++ _posts/2020-12-18/parser.ml
@@ -28,30 +28,32 @@ let yylen = "\002\000\
 
 let yydefred = "\000\000\
 \000\000\000\000\002\000\000\000\006\000\007\000\000\000\000\000\
-\000\000\000\000\001\000\003\000\004\000\005\000"
+\000\000\000\000\001\000\003\000\004\000\000\000"
 
 let yydgoto = "\002\000\
 \006\000\007\000"
 
 let yysindex = "\001\000\
 \003\255\000\000\000\000\003\255\000\000\000\000\008\255\010\255\
-\003\255\003\255\000\000\000\000\000\000\000\000"
+\003\255\003\255\000\000\000\000\000\000\255\254"
 
 let yyrindex = "\000\000\
 \000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
-\000\000\000\000\000\000\000\000\000\000\000\000"
+\000\000\000\000\000\000\000\000\000\000\013\255"
 
 let yygindex = "\000\000\
 \000\000\252\255"
 
-let yytablesize = 15
+let yytablesize = 19
 let yytable = "\008\000\
-\000\000\001\000\000\000\003\000\013\000\014\000\004\000\000\000\
-\005\000\009\000\010\000\009\000\010\000\011\000\012\000"
+\009\000\001\000\000\000\003\000\013\000\014\000\004\000\000\000\
+\005\000\009\000\010\000\009\000\010\000\011\000\012\000\005\000\
+\000\000\005\000\005\000"
 
 let yycheck = "\004\000\
-\255\255\001\000\255\255\001\001\009\000\010\000\004\001\255\255\
-\006\001\002\001\003\001\002\001\003\001\006\001\005\001"
+\002\001\001\000\255\255\001\001\009\000\010\000\004\001\255\255\
+\006\001\002\001\003\001\002\001\003\001\006\001\005\001\003\001\
+\255\255\005\001\006\001"
 
 let yynames_const = "\
   PLUS\000\
@@ -70,45 +72,45 @@ let yyact = [|
 ; (fun __caml_parser_env ->
     let _1 = (Parsing.peek_val __caml_parser_env 1 : 'expr) in
     Obj.repr(
-# 11 "parser.mly"
+# 12 "parser.mly"
              (_1)
-# 76 "parser.ml"
+# 78 "parser.ml"
                : int))
 ; (fun __caml_parser_env ->
     let _1 = (Parsing.peek_val __caml_parser_env 0 : int) in
     Obj.repr(
-# 15 "parser.mly"
+# 16 "parser.mly"
         (_1)
-# 83 "parser.ml"
+# 85 "parser.ml"
                : 'expr))
 ; (fun __caml_parser_env ->
     let _2 = (Parsing.peek_val __caml_parser_env 1 : 'expr) in
     Obj.repr(
-# 16 "parser.mly"
-                         (_2)
-# 90 "parser.ml"
-               : 'expr))
-; (fun __caml_parser_env ->
-    let _1 = (Parsing.peek_val __caml_parser_env 2 : 'expr) in
-    let _3 = (Parsing.peek_val __caml_parser_env 0 : 'expr) in
-    Obj.repr(
 # 17 "parser.mly"
-                     (_1 + _3)
-# 98 "parser.ml"
+                         (_2)
+# 92 "parser.ml"
                : 'expr))
 ; (fun __caml_parser_env ->
     let _1 = (Parsing.peek_val __caml_parser_env 2 : 'expr) in
     let _3 = (Parsing.peek_val __caml_parser_env 0 : 'expr) in
     Obj.repr(
 # 18 "parser.mly"
+                     (_1 + _3)
+# 100 "parser.ml"
+               : 'expr))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : 'expr) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : 'expr) in
+    Obj.repr(
+# 19 "parser.mly"
                       (_1 * _3)
-# 106 "parser.ml"
+# 108 "parser.ml"
                : 'expr))
 ; (fun __caml_parser_env ->
     Obj.repr(
-# 19 "parser.mly"
+# 20 "parser.mly"
           (0)
-# 112 "parser.ml"
+# 114 "parser.ml"
                : 'expr))
 (* Entry main *)
 ; (fun __caml_parser_env -> raise (Parsing.YYexit (Parsing.peek_val __caml_parser_env 0)))
