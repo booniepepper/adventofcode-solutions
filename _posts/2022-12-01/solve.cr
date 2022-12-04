2c9dbfbe6bbbe6e78926f9819c46ec9504f6e902
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-12-03 22:56:56 -0800

    solve part 2

--- _posts/2022-12-01/solve.cr
+++ _posts/2022-12-01/solve.cr
@@ -1,6 +1,12 @@
 lines = File.read "input"
 
-solution_1 = lines.split(/\n\n/).map {|group| group.lines.map(&.to_i).sum}.max
+elves = lines.split(/\n\n/).map {|group| group.lines.map(&.to_i).sum}
+
+solution_1 = elves.max
 
 puts solution_1
+
+solution_2 = elves.sort.last(3).sum
+
+puts solution_2
 
