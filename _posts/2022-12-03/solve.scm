6ae529a7241275652a69c39ff7fd7154c56f2726
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-12-04 21:09:01 -0800

    solve part 2

--- _posts/2022-12-03/solve.scm
+++ _posts/2022-12-03/solve.scm
@@ -25,4 +25,26 @@
                 (go (+ i 1) 0))))
     (score-of-char (go 0 0)))
 
-(print (foldl + 0 (map find-common-char the-lines)))
+(define (sum ns) (foldl + 0 ns))
+
+(print (sum (map find-common-char the-lines)))
+
+(define (find-common-char-3 group)
+    (define a (car group))
+    (define b (cadr group))
+    (define c (caddr group))
+    (define (go i j k)
+        (define (next-non-k)
+            (if (< j (string-length b))
+                (go i (+ j 1) 0)
+                (go (+ i 1) 0 0)))
+        (if (substring=? a b i j 1)
+            (if (substring=? a c i k 1)
+                (substring a i (+ i 1))
+                (if (< k (string-length c))
+                    (go i j (+ k 1))
+                    (next-non-k)))
+            (next-non-k)))
+    (score-of-char (go 0 0 0)))
+
+(print (sum (map find-common-char-3 (chop the-lines 3))))
