ca204e27c94a292ea2846bf94a22a15522bb3cd7
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-12-06 09:48:59 -0800

    Refactor some Kotlin

--- _posts/2022-12-05/solve.kts
+++ _posts/2022-12-05/solve.kts
@@ -16,14 +16,14 @@ data class Command(val n: Int, val from: Int, val to: Int)
 val commands = lines.dropWhile { it != "" }
     .filter { it != "" }
     .map { it.split(' ') }
-    .map { Command(it[1].toInt(), it[3].toInt(), it[5].toInt()) }
+    .map { Command(it[1].toInt(), it[3].toInt() - 1, it[5].toInt() - 1) }
 
 val crates1 = crates.map { ArrayDeque(it) }.toMutableList()
 
 for (command in commands) {
     val (n, from, to) = command
     for (_i in 1..n) {
-        crates1[to-1].addFirst(crates1[from-1].removeFirst())
+        crates1[to].addFirst(crates1[from].removeFirst())
     }
 }
 
@@ -34,12 +34,12 @@ val crates2 = crates.map { it.toMutableList() }.toMutableList()
 for (command in commands) {
     val (n, from, to) = command
 
-    var head = crates2[from-1].take(n).toMutableList()
-    head.addAll(crates2[to-1])
-    crates2[to-1] = head
+    var head = crates2[from].take(n).toMutableList()
+    head.addAll(crates2[to])
+    crates2[to] = head
 
-    var tail = crates2[from-1].drop(n).toMutableList()
-    crates2[from-1] = tail
+    var tail = crates2[from].drop(n).toMutableList()
+    crates2[from] = tail
 }
 
 println(crates2.map { it.first() }.joinToString(""))
