bb4ca3e62b630c0f6b25235baddf4cc7f6a1a7d9
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-12-06 09:47:37 -0800

    Solve part 2

--- _posts/2022-12-05/solve.kts
+++ _posts/2022-12-05/solve.kts
@@ -10,7 +10,6 @@ val crates = (1..34 step 4).map { n ->
             .map { it[n] }
             .filter { it != ' ' }
     }
-    .map { ArrayDeque(it) }
 
 data class Command(val n: Int, val from: Int, val to: Int)
 
@@ -19,12 +18,29 @@ val commands = lines.dropWhile { it != "" }
     .map { it.split(' ') }
     .map { Command(it[1].toInt(), it[3].toInt(), it[5].toInt()) }
 
+val crates1 = crates.map { ArrayDeque(it) }.toMutableList()
+
 for (command in commands) {
     val (n, from, to) = command
     for (_i in 1..n) {
-        crates[to-1].addFirst(crates[from-1].removeFirst())
+        crates1[to-1].addFirst(crates1[from-1].removeFirst())
     }
 }
 
-println(crates.map { it.first() }.joinToString(""))
+println(crates1.map { it.first() }.joinToString(""))
+
+val crates2 = crates.map { it.toMutableList() }.toMutableList()
+
+for (command in commands) {
+    val (n, from, to) = command
+
+    var head = crates2[from-1].take(n).toMutableList()
+    head.addAll(crates2[to-1])
+    crates2[to-1] = head
+
+    var tail = crates2[from-1].drop(n).toMutableList()
+    crates2[from-1] = tail
+}
+
+println(crates2.map { it.first() }.joinToString(""))
 
