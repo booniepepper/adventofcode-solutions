b542f127c0ab5cc627b089cb6e9273ce2b9648cf
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-12-12 00:05:58 -0800

    Solve part 2

--- _posts/2022-12-08/solve.pl
+++ _posts/2022-12-08/solve.pl
@@ -3,17 +3,20 @@
 :- initialization main.
 
 main :-
-    file_lines("input", Trees),
+    file_matrix("input", Trees),
 
     solve_1(Trees, Sum),
     writeln(Sum),
 
+    solve_2(Trees, BestScore),
+    writeln(BestScore),
+
     halt.
 
-% --- Misc utility ---
+% --- Utility ---
 
 rotate(Goal, As, Bs) :-
-    transpose(As, AsRot),
+    transpose(As, AsRot), 
     call(Goal, AsRot, BsRot),
     transpose(BsRot, Bs).
 
@@ -21,6 +24,21 @@ mirror(Goal, As, Bs) :-
     maplist(reverse, As, AsRev),
     call(Goal, AsRev, BsRev),
     maplist(reverse, BsRev, Bs).
+
+split(0, [It|After], [], It, After).
+split(N, [Item|List], Before, It, After) :-
+    NextN is N - 1,
+    split(NextN, List, RestBefore, It, After),
+    Before = [Item|RestBefore].
+
+visible(Tallest, [Tree|Trees], Visibles) :-
+    (   Tree > Tallest
+    ->  visible(Tree, Trees, RestVisibles),
+        Visibles = [1 | RestVisibles]
+    ;   visible(Tallest, Trees, RestVisibles),
+        Visibles = [0 | RestVisibles]
+    ).
+visible(_, _, []).
 
 % --- Stuff for part 1 ---
 
@@ -45,19 +63,62 @@ visible_from_e(Trees, Visibles) :-
     mirror(visible_from_w, Trees, Visibles).
 
 visible_from_w(Trees, Visibles) :-
-    maplist(visible_from_w(-1), Trees, Visibles).
+    maplist(visible(-1), Trees, Visibles).
 
-visible_from_w(Tallest, [Tree|Trees], Visibles) :-
-    Tree > Tallest,
-    visible_from_w(Tree, Trees, RestVisibles),
-    Visibles = [1 | RestVisibles];
-    visible_from_w(Tallest, Trees, RestVisibles),
-    Visibles = [0 | RestVisibles].
-visible_from_w(_, _, []).
+% --- Stuff for Part 2 ---
+
+take_until(_, [], []).
+take_until(Goal, [A|As], Bs) :-
+    (   call(Goal, A)
+    ->  Bs = [A]
+    ;   take_until(Goal, As, RestBs),
+        Bs = [A | RestBs]
+    ).
+
+solve_2(Trees, Score) :-
+    nth0(0, Trees, Row1),
+    length(Row1, NCol),
+    length(Trees, NRow),
+    LastCol is NCol - 2,
+    LastRow is NRow - 2,
+    scenic_score([1, 1], [LastRow, LastCol], 0, Trees, Score).
+
+scenic_score([Row, Col], [LastRow, LastCol], PrevScore, Trees, BestScore) :-
+    split(Row, Trees, AboveRows, TreeRow, BelowRows),
+    maplist(nth0(Col), AboveRows, AboveRev),
+    reverse(AboveRev, Above),
+    maplist(nth0(Col), BelowRows, Below),
+    split(Col, TreeRow, BeforeRev, It, After),
+    reverse(BeforeRev, Before),
+    take_until(=<(It), Above, Ns),
+    take_until(=<(It), Below, Ss),
+    take_until(=<(It), Before, Es),
+    take_until(=<(It), After, Ws),
+    length(Ns, N),
+    length(Ss, S),
+    length(Es, E),
+    length(Ws, W),
+
+    Score is N * S * E * W,
+
+    (   Score > PrevScore
+    ->  NextScore = Score
+    ;   NextScore = PrevScore
+    ),
+
+    (   Col < LastCol
+    ->  NextCol is Col + 1,
+        scenic_score([Row, NextCol], [LastRow, LastCol], NextScore, Trees, BestScore)
+    ;   (   Row < LastRow
+        ->  NextRow is Row + 1,
+            scenic_score([NextRow, 0], [LastRow, LastCol], NextScore, Trees, BestScore)
+        ;   BestScore = NextScore
+        )
+    ).
 
 % --- Reading the file into a number matrix ---
 
-file_lines(File, Matrix) :-
+file_matrix(File, Matrix) :-
     setup_call_cleanup(open(File, read, In),
        stream_matrix(In, Matrix),
        close(In)).
